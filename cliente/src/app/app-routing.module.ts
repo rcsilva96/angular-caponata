d0a513cf1e227b18c899b196b422f2ed924737d2
rcsilva96 <rsilv1822@gmail.com>
2023-11-19 23:11:12 -0300

    Adicionando componentes adicionais, a sidebar já funciona

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -4,12 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 // Componentes
 import { ListarProdutosComponent } from './components/listar-produtos/listar-produtos.component';
 import { CriarProdutoComponent } from './components/criar-produto/criar-produto.component';
+import { HomeComponent } from './components/home/home.component';
+import { VendasComponent } from './components/vendas/vendas.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { RelatorioComponent } from './components/relatorio/relatorio.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
-  { path: '', component: ListarProdutosComponent},
+  { path: '', component: HomeComponent},
+  { path: 'vendas', component: VendasComponent},
+  { path: 'catalogo', component: CatalogoComponent},
+  { path: 'relatorio', component: RelatorioComponent},
+  { path: 'estoque', component: ListarProdutosComponent},
+  { path: 'sobre', component: SobreComponent},
   { path: 'criar-produto', component: CriarProdutoComponent},
   { path: 'editar-produto/:id', component: CriarProdutoComponent},
-  { path: '**', component: ListarProdutosComponent, pathMatch: 'full'}
+  { path: '**', component: NotfoundComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -9,12 +9,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CriarProdutoComponent } from './components/criar-produto/criar-produto.component';
 import { ListarProdutosComponent } from './components/listar-produtos/listar-produtos.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
+import { VendasComponent } from './components/vendas/vendas.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { RelatorioComponent } from './components/relatorio/relatorio.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CriarProdutoComponent,
-    ListarProdutosComponent
+    ListarProdutosComponent,
+    SidebarComponent,
+    HomeComponent,
+    VendasComponent,
+    CatalogoComponent,
+    RelatorioComponent,
+    SobreComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
